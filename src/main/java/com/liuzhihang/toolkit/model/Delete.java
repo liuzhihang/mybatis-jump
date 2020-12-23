215677b84ff96f33674a16ca43a3e3d942538f8b
liuzhihang <liuzhihangs@qq.com>
2020-12-23 23:15:23 +0800

    初始化

---
+++ src/main/java/com/liuzhihang/toolkit/model/Delete.java
@@ -0,0 +1,12 @@
+package com.liuzhihang.toolkit.model;
+
+
+/**
+ * 只需要跳转功能
+ *
+ * @author liuzhihang
+ * @link https://jetbrains.org/intellij/sdk/docs/reference_guide/frameworks_and_external_apis/xml_dom_api.html
+ * @date 2020/7/31 19:20
+ */
+public interface Delete extends Statement {
+}
---
+++ src/main/java/com/liuzhihang/toolkit/model/Mapper.java
@@ -0,0 +1,44 @@
+package com.liuzhihang.toolkit.model;
+
+import com.intellij.util.xml.*;
+
+import java.util.List;
+
+/**
+ * 只需要跳转功能
+ *
+ * @author liuzhihang
+ * @link https://jetbrains.org/intellij/sdk/docs/reference_guide/frameworks_and_external_apis/xml_dom_api.html
+ * @date 2020/7/31 19:20
+ */
+public interface Mapper extends DomElement {
+
+    /**
+     * namespace
+     *
+     * @return
+     */
+    @Attribute("namespace")
+    GenericAttributeValue<String> getNamespace();
+
+    /**
+     * 增删改查对应的节点
+     *
+     * @return
+     */
+    @SubTagsList({"select", "insert", "update", "delete"})
+    List<Statement> getStatements();
+
+    @SubTagList("select")
+    List<Select> getSelects();
+
+    @SubTagList("insert")
+    List<Insert> getInserts();
+
+    @SubTagList("update")
+    List<Update> getUpdates();
+
+    @SubTagList("delete")
+    List<Delete> getDeletes();
+
+}
